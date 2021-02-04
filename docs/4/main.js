a62cf6322abd635a67099c54711ba82e4873684a
ytyaru <ytyaru@outlook.jp>
2021-02-04 10:36:35 +0900

    notify from button

---
+++ docs/4/main.js
@@ -0,0 +1,45 @@
+window.addEventListener('load', (event) => {
+    const App = {
+        setup() {
+            const onClick = ()=>{
+//                alert('今のは痛かった……痛かったぞぉおおお！');
+//                this.notifyClick(); // 参照できない
+//                notifyClick(); // 参照できない
+                /* 参照できない
+                this.$notify({
+                  title: 'ボタンを押した',
+                  message: Vue.h('i', { style: 'color: teal' }, '今のは痛かった……痛かったぞぉおおお！')
+                });
+                */
+            };
+            const setFontSize = (event)=>{
+                console.log(fontSize.value);
+                alert(`フォントサイズを${fontSize.value}に設定します`);
+            };
+            const setWriteDirection = (event)=>{
+                console.log(writeDirection.value);
+                alert(`文字の方向を${writeDirection.value}に設定します`);
+            }
+            const fontSize = Vue.ref('1.4em');
+            const writeDirection= Vue.ref('vertical');
+            return {
+                onClick,
+                setFontSize,
+                setWriteDirection,
+                fontSize,
+                writeDirection,
+            };
+        },
+        methods: {
+            notifyClick() {
+                this.$notify({
+                  title: 'ボタンを押した',
+                  message: Vue.h('i', { style: 'color: teal' }, '今のは痛かった……痛かったぞぉおおお！')
+                });
+            },
+        }
+    };
+    const app = Vue.createApp(App);
+    app.use(ElementPlus);
+    app.mount("#app");
+});
